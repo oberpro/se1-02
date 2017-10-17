16419882caf39b28af862df09e57d290b98d96c9
Sascha0912 <s.obst.so@live.de>
2017-10-17 22:46:21 +0200

    TestCase test_example1_01 hinzugefügt

---
+++ src/main/java/de/oberpro/software_engineering_1_task_02/MyProjectTester.java
@@ -0,0 +1,27 @@
+package de.oberpro.software_engineering_1_task_02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Sascha
+ *
+ */
+public class MyProjectTester extends TestCase
+{
+	Project example1;
+	
+	@Before
+	public void setUp(){
+		example1 = new Project(new String[][]{{"A", "C"}, {"D", "C"}, {"B", "C"}});
+	}
+	
+	@Test
+	  public void test_example1_01() {
+	    assertFalse(example1.isWellSorted(new String[]{"A", "B", "C", "D"}));
+	  }
+}

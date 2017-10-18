0a6e5fc05ef77f51683b428c0b577ba33d97f1dd
Sascha0912 <s.obst.so@live.de>
2017-10-18 14:57:32 +0200

    Update

--- src/test/java/de/oberpro/software_engineering_1_task_02/MyProjectTester.java
+++ src/test/java/de/oberpro/software_engineering_1_task_02/MyProjectTester.java
@@ -1,7 +1,5 @@
 package de.oberpro.software_engineering_1_task_02;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,24 +12,37 @@ import junit.framework.TestCase;
 public class MyProjectTester extends TestCase
 {
 	IfProject example1;
-	
+
+	@Override
 	@Before
-	public void setUp(){
-		example1 = new Project(new String[][]{{"A", "C"}, {"D", "C"}, {"B", "C"}});
+	public void setUp()
+	{
+		example1 = new Project(new String[][]
+		{
+				{ "A", "C" },
+				{ "D", "C" },
+				{ "B", "C" } });
 	}
-	
+
 	@Test
-	  public void test_example1_01() {
-	    assertFalse(example1.isWellSorted(new String[]{"A", "B", "C", "D"}));
-	  }
-	
+	public void test_example1_01()
+	{
+
+		assertFalse(example1.isWellSorted(new String[]
+		{ "A", "B", "C", "D" }));
+	}
+
 	@Test
-	  public void test_example1_02() {
-	    assertFalse(example1.isWellSorted(new String[]{"B", "C", "A", "D"}));
-	  }
-	
+	public void test_example1_02()
+	{
+		assertFalse(example1.isWellSorted(new String[]
+		{ "B", "C", "A", "D" }));
+	}
+
 	@Test
-	  public void test_example1_03() {
-	    assertTrue(example1.isWellSorted(new String[]{"A", "B", "D", "C"}));
-	  }
+	public void test_example1_03()
+	{
+		assertTrue(example1.isWellSorted(new String[]
+		{ "A", "B", "D", "C" }));
+	}
 }
